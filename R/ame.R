1b51c5a5e96f91e0b63a920302e838586d239afa
jBeste <jonas.beste@gmail.com>
2015-12-05 17:51:19 +0100

    correction p-value

--- R/ame.R
+++ R/ame.R
@@ -136,7 +136,7 @@ ame.boot <- function(data.name, rep=100, meth="dt", func, var.name, fromtoby) {
   mean <- round(mean(ame.boot), 3)
   se <- round(sd(ame.boot), 3)
   error <- round((qnorm(0.975) * se), 3)
-  p <- round(2*pnorm(mean/-abs(se)), 3)
+  p <- round(2*pnorm(-abs(mean/se)), 3)
   output <- c(ame=mean, se=se, p=p, lci=mean-error, uci=mean+error)
   return(output)
 }
