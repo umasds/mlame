a5190b040e6c336015b92b572f7fa4356034a8a9
jBeste <jonas.beste@gmail.com>
2015-12-08 23:22:26 +0100

    bootstrap for categorical variables

--- R/ame.R
+++ R/ame.R
@@ -203,31 +203,3 @@ ame <- function(data.name, meth="dt", func, var.name, fromtoby=NULL, plotTree=FA
   }
   return(output)
 }
-<<<<<<< HEAD
-
-# Bootstrapping
-ame.boot <- function(data.name, rep=100, meth="dt", func, var.name, fromtoby) {
-  if (is.factor(data.name[[var.name]]) & length(levels(data.name[[var.name]])>2)) {
-    ame.boot <- replicate(rep, ame(sample_n(data.name, nrow(data.name), replace=T), meth, func, var.name, fromtoby)[1])
-    #mean <- Reduce("+", ame.boot) / length(ame.boot)
-    mean.vec <- apply(simplify2array(ame.boot), 1, mean)
-    mean <- matrix(0, length(mean.vec), length(mean.vec))  
-    mean[lower.tri(mean, diag=FALSE)] <- mean.vec    
-    se.vec <- apply(simplify2array(ame.boot), 1, sd)
-    se <- matrix(0, length(se.vec), length(se.vec))  
-    se[lower.tri(se, diag=FALSE)] <- se.vec
-    p <- round(2*pt(-abs(mean/se), df=rep-1), 3)
-    output <- list(ame=mean, se=se, p=p)    
-  }
-  else{
-    ame.boot <- replicate(rep, unlist(ame(sample_n(data.name, nrow(data.name), replace=T), meth, func, var.name, fromtoby)[1]))
-    mean <- round(mean(ame.boot), 3)
-    se <- round(sd(ame.boot), 3)
-    error <- round((qnorm(0.975) * se), 3)
-    p <- round(2*pt(-abs(mean/se), df=rep-1), 3) # 2*pnorm(-abs(mean/se))
-    output <- c(ame=mean, se=se, p=p, lci=mean-error, uci=mean+error)
-  }
-  return(output)
-}
-=======
->>>>>>> aa7d36d16c3aee086487d2af7f7225aeb6fc9ce9
